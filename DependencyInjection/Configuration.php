580d295162bce5cb100a8d8485d09a43823c4845
tourze <tourze@users.noreply.github.com>
2022-11-01 19:39:51 +0800

    Remove deprecated warning (#8)

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -10,7 +10,7 @@ class Configuration implements ConfigurationInterface
     /**
      * {@inheritDoc}
      */
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         $treeBuilder = new TreeBuilder('intaro_twig_injection');
         if (!\method_exists($treeBuilder, 'getRootNode')) {
