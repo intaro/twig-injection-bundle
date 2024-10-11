10fc67ef9c9caba612039743db237fb49bc4f4ac
Ilyas Salikhov <salikhoff@gmail.com>
2024-10-11 13:32:54 +0300

    fix type in IntaroTwigInjectionExtension

--- DependencyInjection/IntaroTwigInjectionExtension.php
+++ DependencyInjection/IntaroTwigInjectionExtension.php
@@ -12,7 +12,7 @@ class IntaroTwigInjectionExtension extends Extension
     /**
      * {@inheritDoc}
      */
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
